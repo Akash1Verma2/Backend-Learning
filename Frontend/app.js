eba9dc8bfdad696214b5d7514615ced9e29039d7
Akash1Verma2 <ankitverma8621@gmail.com>
2026-03-14 12:54:05 +0530

    feat: Creating and initializing an object using Constructor

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,11 +1,28 @@
-function PersonMaker(name,age) {
-    const person = {
-        name: name,
-        age: age,
-        talk() {
-            console.log(`Hi, my name is ${this.name}`);
-        }
-    }
+// Factory Functions
+// function PersonMaker(name,age) {
+//     const person = {
+//         name: name,
+//         age: age,
+//         talk() {
+//             console.log(`Hi, my name is ${this.name}`);
+//         }
+//     }
 
-    return person;
+//     return person;
+// }
+
+
+// Constructors 
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    console.log(this);
+
 }
+
+Person.prototype.talk = function () {
+    console.log(`Hi, ,my name is ${this.name}`);
+}
+
+let p1 = new Person("adam", 25);
+let p2 = new Person("eve", 25);
