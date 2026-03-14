4ba123693579cdb1bdd86738171023080a4b3d16
Akash1Verma2 <ankitverma8621@gmail.com>
2026-03-14 14:37:15 +0530

    feat: Creating and initializing an object using Inheritance

--- Frontend/app.js
+++ Frontend/app.js
@@ -24,16 +24,51 @@
 //     console.log(`Hi, ,my name is ${this.name}`);
 // }
 
-class Person {
-    constructor(name, age){
+// class Person {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     talk(){
+//         console.log(`Hi, ,my name is ${this.name}`);
+//     }
+// }
+
+// let p1 = new Person("adam", 25);
+// let p2 = new Person("eve", 25);
+
+class Mammal {   
+    constructor(name){
         this.name = name;
-        this.age = age;
+        this.type = "warm-blooded";
     }
 
-    talk(){
-        console.log(`Hi, ,my name is ${this.name}`);
+    eat(){
+        console.log("I am eating");
     }
 }
 
-let p1 = new Person("adam", 25);
-let p2 = new Person("eve", 25);
+class Dog extends Mammal {
+    constructor(name){
+        super(name);
+    }
+
+    bark(){
+        console.log("woff..");
+    }
+
+    eat(){
+        consol.log("dog is eating");
+    }
+}
+
+class Cat extends Mammal {
+    constructor(name) {
+        super(name);
+    }
+
+    meow(){
+        console.log("mewo..")
+    }
+}
